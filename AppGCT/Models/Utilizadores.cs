fd303eae9216129bdc4197fbbcfd206e8498828b
goncalon <goncalosilvan@gmail.com>
2023-03-20 22:28:33 +0000

    Adição MODEL Utilizadores Inicial

---
+++ AppGCT/Models/Utilizadores.cs
@@ -0,0 +1,11 @@
+﻿namespace AppGCT.Models
+{
+    public class Utilizadores
+    {
+        public int id {  get; set; }
+        public int? FK_idRole { get; set; }
+        public int estadoUtilizador { get; set; }
+        public string nome { get; set; }
+
+    }
+}
