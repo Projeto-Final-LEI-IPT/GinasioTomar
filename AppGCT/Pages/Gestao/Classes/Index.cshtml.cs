e0b7076e52e37e442135f3d39e9d644b37c6a818
jonicorreia <joni.correia.1990@gmail.com>
2023-07-02 16:26:07 +0100

    Inclusão de filtro e sort's no Index.html das Classes

--- AppGCT/Pages/Gestao/Classes/Index.cshtml.cs
+++ AppGCT/Pages/Gestao/Classes/Index.cshtml.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using AppGCT.Data;
 using AppGCT.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNet.Identity;
 
 namespace AppGCT.Pages.Gestao.Classes
 {
@@ -20,14 +21,45 @@ namespace AppGCT.Pages.Gestao.Classes
         {
             _context = context;
         }
-
+        public string NomeClasseSort { get; set; }
+        public string EstadoClasseSort { get; set; }
+        public string CurrentFilter { get; set; }
         public IList<Classe> Classe { get;set; } = default!;
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string sortOrder, string searchString)
         {
             if (_context.Classe != null)
             {
-                Classe = await _context.Classe.ToListAsync();
+               // Classe = await _context.Classe.ToListAsync();
+                NomeClasseSort = String.IsNullOrEmpty(sortOrder) ? "nomeclasse_desc" : "";
+                EstadoClasseSort = sortOrder == "EstadoClasse" ? "estadoclasse_desc" : "EstadoClasse";
+
+                CurrentFilter = searchString;
+
+                IQueryable<Classe> ClasseIQ = _context.Classe.AsQueryable();
+
+                if (!String.IsNullOrEmpty(searchString))
+                {
+                    ClasseIQ = ClasseIQ.Where(s => s.NomeClasse.ToUpper().Contains(searchString.ToUpper()));
+                }
+
+                switch (sortOrder)
+                {
+                    case "nomeclasse_desc":
+                        ClasseIQ = ClasseIQ.OrderByDescending(s => s.NomeClasse);
+                        break;
+                    case "EstadoClasse":
+                        ClasseIQ = ClasseIQ.OrderBy(s => s.EstadoClasse);
+                        break;
+                    case "estadoclasse_desc":
+                        ClasseIQ = ClasseIQ.OrderByDescending(s => s.EstadoClasse);
+                        break;
+                    default:
+                        ClasseIQ = ClasseIQ.OrderBy(s => s.NomeClasse);
+                        break;
+                }
+
+                Classe = await ClasseIQ.ToListAsync();
             }
         }
     }
--- AppGCT/Pages/Gestao/Epocas/Index.cshtml.cs
+++ AppGCT/Pages/Gestao/Epocas/Index.cshtml.cs
@@ -26,59 +26,55 @@ namespace AppGCT.Pages.Ginasio.Epocas
         public string DataFimSort { get; set; }
         public string EstadoEpocaSort { get; set; }
         public string CurrentFilter { get; set; }
-        public string CurrentSort { get; set; }
         public IList<Epoca> Epoca { get; set; } = default!;
 
         public async Task OnGetAsync(string sortOrder, string searchString)
         {
             if (_context.Epoca != null)
             {
-                {
-                    EpocaSort = String.IsNullOrEmpty(sortOrder) ? "epoca" : "";
-                    DataInicioSort = sortOrder == "DataInicio" ? "datainicio_desc" : "DataInicio";
-                    DataFimSort = sortOrder == "DataFim" ? "datafim_desc" : "DataFim";
-                    EstadoEpocaSort = sortOrder == "EstadoEpoca" ? "estadoepoca_desc" : "EstadoEpoca";
+               EpocaSort = String.IsNullOrEmpty(sortOrder) ? "epoca" : "";
+               DataInicioSort = sortOrder == "DataInicio" ? "datainicio_desc" : "DataInicio";
+               DataFimSort = sortOrder == "DataFim" ? "datafim_desc" : "DataFim";
+               EstadoEpocaSort = sortOrder == "EstadoEpoca" ? "estadoepoca_desc" : "EstadoEpoca";
 
-                    CurrentFilter = searchString;
+               CurrentFilter = searchString;
 
-                    string userId = User.Identity.GetUserId();
-                    IQueryable<Epoca> EpocaIQ = _context.Epoca.AsQueryable();
+               IQueryable<Epoca> EpocaIQ = _context.Epoca.AsQueryable();
 
-                    if (!String.IsNullOrEmpty(searchString))
-                    {
-                        EpocaIQ = EpocaIQ.Where(s => s.NomeEpoca.ToUpper().Contains(searchString.ToUpper()));
-                    }
+               if (!String.IsNullOrEmpty(searchString))
+               {
+                  EpocaIQ = EpocaIQ.Where(s => s.NomeEpoca.ToUpper().Contains(searchString.ToUpper()));
+               }
 
-                    switch (sortOrder)
-                    {
-                        case "epoca":
-                            EpocaIQ = EpocaIQ.OrderBy(s => s.NomeEpoca);
-                            break;
-                        case "DataInicio":
-                            EpocaIQ = EpocaIQ.OrderBy(s => s.DataInicio);
-                            break;
-                        case "datainicio_desc":
-                            EpocaIQ = EpocaIQ.OrderByDescending(s => s.DataInicio);
-                            break;
-                        case "DataFim":
-                            EpocaIQ = EpocaIQ.OrderBy(s => s.DataFim);
-                            break;
-                        case "datafim_desc":
-                            EpocaIQ = EpocaIQ.OrderByDescending(s => s.DataFim);
-                            break;
-                        case "EstadoEpoca":
-                            EpocaIQ = EpocaIQ.OrderBy(s => s.EstadoEpoca);
-                            break;
-                        case "estadoepoca_desc":
-                            EpocaIQ = EpocaIQ.OrderByDescending(s => s.EstadoEpoca);
-                            break;
-                        default:
-                            EpocaIQ = EpocaIQ.OrderByDescending(s => s.NomeEpoca);
-                            break;
-                    }
+               switch (sortOrder)
+               {
+                   case "epoca":
+                        EpocaIQ = EpocaIQ.OrderBy(s => s.NomeEpoca);
+                        break;
+                   case "DataInicio":
+                        EpocaIQ = EpocaIQ.OrderBy(s => s.DataInicio);
+                        break;
+                   case "datainicio_desc":
+                        EpocaIQ = EpocaIQ.OrderByDescending(s => s.DataInicio);
+                        break;
+                   case "DataFim":
+                        EpocaIQ = EpocaIQ.OrderBy(s => s.DataFim);
+                        break;
+                   case "datafim_desc":
+                        EpocaIQ = EpocaIQ.OrderByDescending(s => s.DataFim);
+                        break;
+                   case "EstadoEpoca":
+                        EpocaIQ = EpocaIQ.OrderBy(s => s.EstadoEpoca);
+                        break;
+                   case "estadoepoca_desc":
+                        EpocaIQ = EpocaIQ.OrderByDescending(s => s.EstadoEpoca);
+                        break;
+                   default:
+                        EpocaIQ = EpocaIQ.OrderByDescending(s => s.NomeEpoca);
+                        break;
+               }
 
                     Epoca = await EpocaIQ.ToListAsync();
-                }
             }
         }
     }
