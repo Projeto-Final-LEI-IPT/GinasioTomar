ace3c088e763746765b63adb8b47d069334f91fc
jonicorreia <joni.correia.1990@gmail.com>
2023-10-07 00:10:01 +0100

    commit

---
+++ AppGCT/Pages/Estatisticas/Index.cshtml.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AppGCT.Pages.Estatisticas
+{
+    [Authorize(Roles = "Administrador,Ginásio")]
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
