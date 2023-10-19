00db70500cd5484c2ef5c850e834b0f5bef44883
jonicorreia <joni.correia.1990@gmail.com>
2023-10-19 19:20:21 +0100

    Reverter secrets do pass Admin

--- AppGCT/Data/AppGCTContext.cs
+++ AppGCT/Data/AppGCTContext.cs
@@ -31,11 +31,11 @@ namespace AppGCT.Data
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
-            var configuration = new ConfigurationBuilder()
-                                    .AddUserSecrets<Program>()
-                                    .Build();
+            //var configuration = new ConfigurationBuilder()
+            //                        .AddUserSecrets<Program>()
+            //                        .Build();
 
-            builder.Seed(configuration);
+            //builder.Seed(configuration);
             //define campo Id do Identity de 36 posições (GUID)
             builder.Entity<Utilizador>(b =>
             {
